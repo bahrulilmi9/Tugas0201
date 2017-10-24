dfffcb9bf8a70b237116bf01126ad01a5914519c
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-10-24 13:55:25 +0800

    Output inisialisasi nilai

--- tugas4.cpp
+++ tugas4.cpp
@@ -5,7 +5,12 @@ int main(){
 
 	double LuasRumah = 42;
 	double LantaiPkt = 193*1215;
-	double LuasBox = LantaiPkt*8/1000000;
+	double Luas1Box = LantaiPkt*8/100000;
 	double JumlahBoxP, BiayaP, BiayaPG,TotalB;
+	
+	cout << "Luas Rumah				   :"<<LuasRumah<<" m2"<<endl;
+	cout << "Luas 1 box parket		   :"<<Luas1Box<<" m2"<<endl;
+	
+	
 	
 }

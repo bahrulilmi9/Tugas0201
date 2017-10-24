64568d63bda31bce2659566fdc22ddbd3c46937c
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-10-24 13:45:42 +0800

    Menginisialisasi Nilai

--- tugas4.cpp
+++ tugas4.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
 
-	
+	double LuasRumah = 42;
+	double LantaiPkt = 193*1215;
+	double LuasBox = LantaiPkt*8/1000000;
+	double JumlahBoxP, BiayaP, BiayaPG,TotalB;
 	
 }
